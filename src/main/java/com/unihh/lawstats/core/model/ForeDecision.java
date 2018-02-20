9addccf613a9b444c7cd3d08fee8bcb46a57f1e2
Tjorben Gschwander <tjorben.gschwander@mgm-tp.com>
2018-02-20 13:14:53 +0100

    fill base datamodel

---
+++ src/main/java/com/unihh/lawstats/core/model/ForeDecision.java
@@ -0,0 +1,9 @@
+package com.unihh.lawstats.core.model;
+
+import java.util.Date;
+
+public class ForeDecision {
+
+    private String court;
+    private Date dateVerdict;
+}
--- src/main/java/com/unihh/lawstats/core/model/Verdict.java
+++ src/main/java/com/unihh/lawstats/core/model/Verdict.java
@@ -1,7 +1,23 @@
 package com.unihh.lawstats.core.model;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * This class represents one document with the extracted date from watson
  */
 public class Verdict {
+
+    private String docketNumber;
+    private int revisionSuccess;
+    private String senate;
+    private List<String> judgeList;
+    private Date dateVerdict;
+    //Oberlandesgericht
+    private ForeDecision foreDecisionRAC;
+    //Landesgericht
+    private ForeDecision foreDecisionRC;
+    //Amtsgericht
+    private ForeDecision foreDecisionDC;
+
 }
