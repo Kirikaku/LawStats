5ed9e6961eff15c6460112a07858f2d16fa3b01e
Phillip <phillip.sittig@web.de>
2018-02-20 18:12:36 +0100

    Download of verdicts from Bundesgerichtshof. Start and end index can be specified.

---
+++ src/main/java/com/unihh/lawstats/bootstrap/VerdictDownloader.java
@@ -0,0 +1,31 @@
+package com.unihh.lawstats.bootstrap;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class VerdictDownloader {
+
+    public void downloadVerdicts() throws Exception{
+        int startIndex = 17756;
+        int endIndex = 80984;
+
+
+
+        for(int i = startIndex; i <= endIndex; i++) {
+
+            String urlString = "http://juris.bundesgerichtshof.de/cgi-bin/rechtsprechung/document.py?Gericht=bgh&Art=en&Datum=Aktuell&nr="+i+"&Frame=4&.pdf";
+            String filePath = "C:\\Users\\Phillip\\Documents\\verdicts\\verdict"+i+".pdf";
+
+            URL url = new URL(urlString);
+            BufferedInputStream in = new BufferedInputStream(url.openStream());
+            Files.copy(in, Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+            in.close();
+        }
+    }
+
+
+
+}
