d182dbe3b6b5120f509248ae08962476fe037864
tobikilling <tobias.killing@outlook.com>
2018-02-22 15:16:28 +0100

    Website

--- src/main/java/com/unihh/lawstats/backend/ImportTestData.java
+++ src/main/java/com/unihh/lawstats/backend/ImportTestData.java
@@ -1,4 +1,4 @@
-package com.unihh.lawstats.backend;
+/* package com.unihh.lawstats.backend;
 
 import com.unihh.lawstats.backend.repositories.VerdictRepository;
 import com.unihh.lawstats.core.model.Verdict;
@@ -80,3 +80,4 @@ public class ImportTestData {
 
 
 }
+ */
