7462ff2e61d8a19a843eecf252f15392d54f6a09
Tjorben Gschwander <tjorben.gschwander@mgm-tp.com>
2018-02-26 12:32:45 +0100

    add attributes enum for the verdict product

---
+++ src/main/java/com/unihh/lawstats/core/model/Attributes.java
@@ -0,0 +1,7 @@
+package com.unihh.lawstats.core.model;
+
+public enum Attributes {
+
+    DocketNumber, RevisionSuccess, Senate, Judges, DateVerdict, ForeDecisionRACCourt, ForeDecisionRACDateVerdict,
+    ForeDecisionRCCourt, ForeDecisionRCDateVerdict, ForeDecisionDCCourt, ForeDecisionDCDateVerdict
+}
