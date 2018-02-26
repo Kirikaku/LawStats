9a6a500ef2c86a4ba4be6e8988df0920f8362e20
Tjorben Gschwander <tjorben.gschwander@mgm-tp.com>
2018-02-26 19:47:13 +0100

    add settings for bootstrapping phase for getting solrBean

---
+++ src/main/java/com/unihh/lawstats/bootstrap/Mein.java
@@ -0,0 +1,16 @@
+package com.unihh.lawstats.bootstrap;
+
+import com.unihh.lawstats.backend.repositories.VerdictRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+// This is the code snippet for getting the verdict repo in the bootstrapping phse
+public class Mein {
+
+    public void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beansDefinitionForBootstrapPhase.xml");
+        VerdictRepository verdictRepository = context.getBean("verdictRepository", VerdictRepository.class);
+
+
+        System.out.println("Test");
+    }
+}
