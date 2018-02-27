cd6c617e57fb573f3959d626373cc41123574ffe
Phillip <phillip.sittig@web.de>
2018-02-27 13:01:22 +0100

    add lawclassifier

--- src/main/java/com/unihh/lawstats/bootstrap/Watson/LawClassifier.java
+++ src/main/java/com/unihh/lawstats/bootstrap/Watson/LawClassifier.java
@@ -2,20 +2,25 @@ package com.unihh.lawstats.bootstrap.Watson;
 
 
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.NaturalLanguageClassifier;
+import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classification;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classifier;
 
 import java.io.File;
 
-public class Classification {
+public class LawClassifier {
     NaturalLanguageClassifier _service = new NaturalLanguageClassifier();
 
     public static void main(String[] args) {
-        Classification classification = new Classification();
-        classification.trainClassifier();
+
+
+        //LawClassifier lawClassifier = new LawClassifier();
+        LawEntityExtractor lawEntityExtractor = new LawEntityExtractor();
+        //lawClassifier.trainClassifier();
+        lawEntityExtractor.extractEntities("dude");
     }
 
-    public Classification() {
-        this._service.setUsernameAndPassword("\"056d44aa-83e3-4adf-9c57-c6f006164a2b\"", "\"5EaHAYwFVLEc\"");
+    public LawClassifier() {
+        this._service.setUsernameAndPassword("056d44aa-83e3-4adf-9c57-c6f006164a2b", "5EaHAYwFVLEc");
     }
 
     public void trainClassifier() {
@@ -29,4 +34,16 @@ public class Classification {
         Classifier classifier = (Classifier) this._service.createClassifier("PerformanceTestClassifier", "de", new File("C:\\Users\\Phillip\\Documents\\Studium\\Praktikum Sprachtechnologie\\IBM Classifier\\weather_data_train.csv")).execute();
         System.out.println(classifier);
     }
+
+
+
+    public void classifySentence(String classifierId, String sentence){
+        Classification classification = _service.classify(classifierId, sentence).execute();
+
+
+    }
+
+
+
+
 }
