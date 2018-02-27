7ea8569f7a655ae59504d9ccf8c29e30e4ed70a6
Tjorben Gschwander <tjorben.gschwander@mgm-tp.com>
2018-02-27 10:13:06 +0100

    delete unnecessery controller

--- src/main/java/com/unihh/lawstats/bootstrap/Mein.java
+++ src/main/java/com/unihh/lawstats/bootstrap/Mein.java
@@ -3,6 +3,8 @@ package com.unihh.lawstats.bootstrap;
 import com.unihh.lawstats.backend.repositories.VerdictRepository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 // This is the code snippet for getting the verdict repo in the bootstrapping phse
 public class Mein {
 
