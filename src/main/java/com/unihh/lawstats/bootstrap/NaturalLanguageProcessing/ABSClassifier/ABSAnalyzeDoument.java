19b36ec8cbc6205e7a6fdff6fb031171487f14c9
Tjorben Gschwander <tjorben.gschwander@mgm-tp.com>
2018-03-07 11:49:50 +0100

    add analyze result method

--- src/main/java/com/unihh/lawstats/bootstrap/NaturalLanguageProcessing/ABSClassifier/ABSAnalyzeDoument.java
+++ src/main/java/com/unihh/lawstats/bootstrap/NaturalLanguageProcessing/ABSClassifier/ABSAnalyzeDoument.java
@@ -1,4 +1,13 @@
 package com.unihh.lawstats.bootstrap.NaturalLanguageProcessing.ABSClassifier;
 
+import com.unihh.lawstats.core.model.Verdict;
+import uhh_lt.ABSA.ABSentiment.type.Result;
+
+import java.util.List;
+
 public class ABSAnalyzeDoument {
+
+    public Verdict analyzeABSResultsAndPutItInVerdict(List<Result> resultList, Verdict verdict){
+        return null;
+    }
 }
