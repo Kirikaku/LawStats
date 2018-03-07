786203538be12f96287bd5d8b073f6d2d80cc661
Phillip <phillip.sittig@web.de>
2018-03-07 12:05:18 +0100

    Introduction of Models files

--- src/main/java/com/unihh/lawstats/bootstrap/NaturalLanguageProcessing/ABSClassifier/ABSAnalyzeDoument.java
+++ src/main/java/com/unihh/lawstats/bootstrap/NaturalLanguageProcessing/ABSClassifier/ABSAnalyzeDoument.java
@@ -5,7 +5,32 @@ import uhh_lt.ABSA.ABSentiment.type.Result;
 
 import java.util.List;
 
+import com.unihh.lawstats.bootstrap.NaturalLanguageProcessing.NLPLawUtils;
+import uhh_lt.ABSA.ABSentiment.AbSentiment;
+import uhh_lt.ABSA.ABSentiment.type.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ABSAnalyzeDoument {
+
+
+
+    public List<Result> retrieveResultsForDocumentString(String documentText){
+        NLPLawUtils nlpLawUtils = new NLPLawUtils();
+        AbSentiment abSentiment = new AbSentiment("resources/config/ABSConfiguration.txt");
+        List<Result> resultList = new ArrayList<>();
+        List<String> sentenceList = nlpLawUtils.splitDocumentIntoSpecifiedSentences(documentText, 10);
+
+
+        for(String sentence: sentenceList){
+           Result result = abSentiment.analyzeText(sentence);
+           resultList.add(result);
+        }
+
+       return resultList;
+    }
+
 
     public Verdict analyzeABSResultsAndPutItInVerdict(List<Result> resultList, Verdict verdict){
         return null;
