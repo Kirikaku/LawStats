9d64b35eb3ea345002b5bc3bbb8fe19564a2be54
Phillip <phillip.sittig@web.de>
2018-03-13 15:02:54 +0100

    Major refactoring of method findDecisionSentence

--- src/main/java/com/unihh/lawstats/bootstrap/NaturalLanguageProcessing/ABSClassifier/TrainingsdataManagerHelper.java
+++ src/main/java/com/unihh/lawstats/bootstrap/NaturalLanguageProcessing/ABSClassifier/TrainingsdataManagerHelper.java
@@ -13,106 +13,136 @@ import java.util.Map;
 public class TrainingsdataManagerHelper {
 
     public Map<String, String[]> findDecisionSentences(String jsonString) {
+
         JSONObject jsonObject = new JSONObject(jsonString);
-        Map<String, String[]> _decisionSentenceMap = new HashMap<>();
+        Map<String, String[]> decisionSentenceMap = new HashMap<>();
         NLPLawUtils nlpLawUtils = new NLPLawUtils();
+        List<String> firstSentences;
+        String fileName;
+        String fullDocumentText;
+        JSONArray mentionsJsonArray;
+        Iterator<Object> iterableMentionsJsonArray;
 
 
-        String fullDocumentText = jsonObject.getString("text");
+        fileName = jsonObject.getString("name");
+
+
+        fullDocumentText = jsonObject.getString("text");
         fullDocumentText = Formatter.replaceNewLines(fullDocumentText);
+        firstSentences = nlpLawUtils.splitDocumentIntoSpecifiedSentences(fullDocumentText, 5); //TODO evtl. properties
 
-        String fileName = jsonObject.getString("name");
-        JSONArray mentionsJsonArray = jsonObject.getJSONArray("mentions");
-        //jsonObject.remove("mentions");
-
-
-        Iterator<Object> iterableMentionsJsonArray = mentionsJsonArray.iterator();
+        mentionsJsonArray = jsonObject.getJSONArray("mentions");
+        iterableMentionsJsonArray = mentionsJsonArray.iterator();
 
 
-        List<String> _firstSentences = nlpLawUtils.splitDocumentIntoSpecifiedSentences(fullDocumentText, 5);
 
 
-        for(String earlySentence: _firstSentences){
-            String[] valueArray = new String[2];
-            valueArray[0]="irrelevant";
-            valueArray[1]=fileName;
-            String formattedSentence = earlySentence.replaceAll("\\s", " ");
-            //earlySentence = earlySentence.replaceAll("\\r", " ");
-            formattedSentence = formattedSentence.trim();
-
-            _decisionSentenceMap.put(formattedSentence, valueArray);
-        }
-
-
+        initialDefaultFill(firstSentences, decisionSentenceMap, fileName);
 
 
         while (iterableMentionsJsonArray.hasNext()) {
             JSONObject mentionObject = (JSONObject) iterableMentionsJsonArray.next();
 
-            String type = mentionObject.getString("type");
+            String mentionType;
             String sentence = null;
             String revisionType = null;
-            int beginIndex = -1;
-            int endIndex = -1;
 
 
-            if(type.contains("Revision")){
-                beginIndex = mentionObject.getInt("begin");
-                endIndex = mentionObject.getInt("end");
-                sentence = retrieveSentenceFromDocument(fullDocumentText, beginIndex, endIndex);
+
+            mentionType = mentionObject.getString("type");
 
 
-                switch(type.toLowerCase()){
-                    case "revisionsteilerfolg":
-                        revisionType = "revisionsTeilerfolg";
-                        break;
-                    case "revisionserfolg":
-                        revisionType = "revisionsErfolg";
-                        break;
-                    case "revisionsmisserfolg":
-                        revisionType = "revisionsMisserfolg";
-                        break;
-                }
-
+            if(mentionType.contains("Revision")){
 
                 boolean isInMap = false;
 
+                sentence = retrieveMentionSentenceFromDocument(fullDocumentText, mentionObject);
 
+                revisionType = determineRevisionType(mentionType);
 
-                for(Map.Entry<String, String[]> sentenceEntry: _decisionSentenceMap.entrySet()){
-                    if(sentenceEntry.getKey().contains(sentence)){
-                        String[] sentenceValues = sentenceEntry.getValue();
-                        sentenceValues[0] = revisionType;
-                        isInMap = true;
-                    }
-
-
-                }
-
-                if(!isInMap){
-                    String[] valueArray = new String[2];
-                    valueArray[0] = revisionType;
-                    valueArray[1] = fileName;
-                    _decisionSentenceMap.put(sentence, valueArray);
-                }
-
+                updateDecisionSentenceList(decisionSentenceMap, sentence, revisionType, fileName);
 
             }
 
         }
 
 
-        return _decisionSentenceMap;
+
+        return decisionSentenceMap;
+    }
+
+
+    private void initialDefaultFill(List<String> firstSentences, Map<String, String[]> decisionSentenceMap, String fileName){
+
+        for(String earlySentence: firstSentences){
+            String[] valueArray = new String[2];
+            valueArray[0]="irrelevant";
+            valueArray[1]=fileName;
+
+            String formattedSentence = earlySentence.replaceAll("\\s", " ");
+            formattedSentence = formattedSentence.trim();
+
+            decisionSentenceMap.put(formattedSentence, valueArray);
+        }
+
+    }
+
+
+    private void updateDecisionSentenceList(Map<String, String[]> decisionSentenceMap, String sentence, String revisionType, String fileName){
+        boolean isInMap = false;
+
+
+        for(Map.Entry<String, String[]> sentenceEntry: decisionSentenceMap.entrySet()){
+            if(sentenceEntry.getKey().contains(sentence)){
+                String[] sentenceValues = sentenceEntry.getValue();
+                sentenceValues[0] = revisionType;
+                isInMap = true;
+            }
+        }
+
+
+        if(!isInMap){
+            String[] valueArray = new String[2];
+            valueArray[0] = revisionType;
+            valueArray[1] = fileName;
+            decisionSentenceMap.put(sentence, valueArray);
+        }
+    }
+
+
+    private String retrieveMentionSentenceFromDocument(String documentText, JSONObject mentionObject){
+        int beginIndex = mentionObject.getInt("begin");
+        int endIndex = mentionObject.getInt("end");
+
+        String sentence = documentText.substring(beginIndex, endIndex);
+
+        return sentence;
+    }
+
+    private String determineRevisionType(String mentionType){
+        String revisionType = null;
+
+
+        switch(mentionType.toLowerCase()){
+            case "revisionsteilerfolg":
+                revisionType = "revisionsTeilerfolg";
+                break;
+            case "revisionserfolg":
+                revisionType = "revisionsErfolg";
+                break;
+            case "revisionsmisserfolg":
+                revisionType = "revisionsMisserfolg";
+                break;
+        }
+
+        return revisionType;
     }
 
 
 
+    private List<String[]> analyzeMentions(Iterator<Object> mentionsIterator){
 
-
-    private String retrieveSentenceFromDocument(String documentText, int beginIndex, int endIndex){
-        String sentence = documentText.substring(beginIndex, endIndex);
-
-        return sentence;
+        return null;
     }
 
 }
