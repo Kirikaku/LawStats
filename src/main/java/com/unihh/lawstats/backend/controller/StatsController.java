4aa0be9721b36f673567c1b92495686ddda0d3ea
Phillip <phillip.sittig@web.de>
2018-06-19 21:25:23 +0200

    refactored stats controller

--- src/main/java/com/unihh/lawstats/backend/controller/StatsController.java
+++ src/main/java/com/unihh/lawstats/backend/controller/StatsController.java
@@ -34,15 +34,4 @@ public class StatsController {
     }
 
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/stats/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
-    }
-
-
-
 }
